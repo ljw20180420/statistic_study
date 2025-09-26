99a57a68575dc27affa8300afb27f38b4bc5ebc1
ljw <ljw2017@sjtu.edu.cn>
2025-09-26 16:06:11 +0800

    first commit for statistic study

---
+++ process.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+import re
+import pandas as pd
+import pathlib
+
+
+def get_data(data_dir: os.PathLike) -> pd.DataFrame:
+    data_dir = pathlib.Path(os.fspath(data_dir))
+    dfs = []
+    for file in os.listdir(data_dir):
+        if (
+            not re.search(r"^36t-", file)
+            and not re.search(r"^B2-", file)
+            and not re.search(r"^A2-", file)
+            and not re.search(r"^D2-", file)
+            and not re.search(r"^i10t-", file)
+        ):
+            continue
+
+        df = pd.read_csv(data_dir / file, header=0)
+        if re.search(r"^36t-", file) or re.search(r"^B2-", file):
+            df["cas"] = "spymac"
+        elif re.search(r"^A2-", file) or re.search(r"^D2-", file):
+            df["cas"] = "spycas9"
+        elif re.search(r"^i10t-", file):
+            df["cas"] = "ispymac"
+        else:
+            raise ValueError("Unexpected sample")
+
+        mat = re.search(r"([^-]+)-[^-\d](\d)n?+-(\d)-wt(\d)", file)
+        df["cellline"] = mat.group(1)
+        df["chip"] = int(mat.group(2))
+        df["bio_rep"] = int(mat.group(3))
+        df["tech_rep"] = int(mat.group(4))
+
+        dfs.append(df)
+
+    df = (
+        (pd.concat(dfs).groupby(["sgrna", "cas"])["tem_1"].mean())
+        .reset_index(drop=False)
+        .pivot(
+            columns=["cas"],
+            values="tem_1",
+            index="sgrna",
+        )
+    )
+
+    return df
---
+++ study.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+from scipy.stats import (
+    ttest_ind,
+    mannwhitneyu,
+    ttest_rel,
+    wilcoxon,
+    binomtest,
+    bws_test,
+    ranksums,
+    brunnermunzel,
+)
+from scipy.stats import PermutationMethod
+from process import get_data
+
+df = get_data("files_for_templated")
+a = df["spycas9"]
+result_dict = {"b": [], "method": [], "alternative": [], "p-value": []}
+for cas in ["spymac", "ispymac"]:
+    b = df[cas]
+
+    #####################################
+    # The t-test on independent samples (or the so-called unpaired t-test).
+    #####################################
+
+    # equal_var=True performs normal unpaired t-test.
+    # nan_policy="omit" omit missing data.
+    # alternative="greater" means a is smaller than b, which decrease p-value.
+    result_dict["b"].append(cas)
+    result_dict["method"].append("t-test-ind")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        ttest_ind(
+            a, b, equal_var=True, nan_policy="omit", alternative=alternative
+        ).pvalue.item()
+    )
+
+    # equal_var=False performs Welch’s t-test.
+    result_dict["b"].append(cas)
+    result_dict["method"].append("Welch-test")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        ttest_ind(
+            a, b, equal_var=False, nan_policy="omit", alternative=alternative
+        ).pvalue.item()
+    )
+
+    # trim=0.05 performs a trimmed (Yuen’s) t-test, which is an extension of Welch’s t-test and help to filter outliers.
+    result_dict["b"].append(cas)
+    result_dict["method"].append("Yuen-test")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        ttest_ind(
+            a, b, nan_policy="omit", alternative=alternative, trim=0.05
+        ).pvalue.item()
+    )
+
+    # # method=PermutationMethod(n_resamples=9999) performs permutation test over t-statistics.
+    # result_dict["b"].append(cas)
+    # result_dict["method"].append("permute-t-test")
+    # alternative = "greater"
+    # result_dict["alternative"].append(alternative)
+    # result_dict["p-value"].append(
+    #     ttest_ind(
+    #         a,
+    #         b,
+    #         nan_policy="omit",
+    #         alternative=alternative,
+    #         method=PermutationMethod(n_resamples=9999),
+    #     ).pvalue.item()
+    # )
+
+    #####################################
+    # The U-test on independent samples (or non-parametric version of the t-test for independent samples.)
+    #####################################
+
+    # The Mann-Whitney U test requires that the underlying distributions of two samples are the same. It does not care what the underlying distribution is.
+    result_dict["b"].append(cas)
+    result_dict["method"].append("U-test")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        mannwhitneyu(a, b, alternative=alternative, nan_policy="omit").pvalue.item()
+    )
+
+    # # method=PermutationMethod(n_resamples=9999) performs permutation test over U-statistics.
+    # result_dict["b"].append(cas)
+    # result_dict["method"].append("permute-U-test")
+    # alternative = "greater"
+    # result_dict["alternative"].append(alternative)
+    # result_dict["p-value"].append(
+    #     mannwhitneyu(
+    #         a,
+    #         b,
+    #         alternative=alternative,
+    #         nan_policy="omit",
+    #         method=PermutationMethod(n_resamples=9999),
+    #     ).pvalue.item()
+    # )
+
+    ######################################
+    # The binomial test.
+    ######################################
+    result_dict["b"].append(cas)
+    result_dict["method"].append("binomial-test")
+    alternative = "less"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        binomtest(
+            (a > b).sum().item(), len(a), p=0.5, alternative=alternative
+        ).pvalue.item()
+    )
+
+    #######################################
+    # The t-test on two related samples (or the so-called paired t-test).
+    #######################################
+    result_dict["b"].append(cas)
+    result_dict["method"].append("t-test-rel")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        ttest_rel(a, b, nan_policy="omit", alternative=alternative).pvalue.item()
+    )
+
+    ###########################################
+    # The Wilcoxon signed-rank test (non-parametric version of the paired t-test).
+    ###########################################
+    result_dict["b"].append(cas)
+    result_dict["method"].append("Wilcoxon-signed-rank-test")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        wilcoxon(
+            a,
+            b,
+            alternative=alternative,
+            method="auto",
+            nan_policy="omit",
+        ).pvalue.item()
+    )
+
+    # ############################################
+    # # The Baumgartner-Weiss-Schindler test on two independent samples (cannot handle missing data).
+    # ############################################
+    # result_dict["b"].append(cas)
+    # result_dict["method"].append("Baumgartner-Weiss-Schindler-test")
+    # alternative = "greater"
+    # result_dict["alternative"].append(alternative)
+    # bws_test(a, b, alternative=alternative)
+
+    ################################################
+    # The Wilcoxon rank-sum test for two samples. The null hypothesis that two sets of measurements are drawn from the same distribution.
+    ################################################
+    result_dict["b"].append(cas)
+    result_dict["method"].append("Wilcoxon-rank-sum-test")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        ranksums(a, b, alternative=alternative, nan_policy="omit").pvalue.item()
+    )
+
+    #################################################
+    # The Brunner-Munzel test on two independent samples. It does not require the equal variance (like the Wilcoxon-Mann-Whitney’s U-test,) and same distribution.
+    #################################################
+    result_dict["b"].append(cas)
+    result_dict["method"].append("Brunner-Munzel-test")
+    alternative = "greater"
+    result_dict["alternative"].append(alternative)
+    result_dict["p-value"].append(
+        brunnermunzel(a, b, alternative=alternative, nan_policy="omit").pvalue.item()
+    )
+
+
+result_df = pd.DataFrame(result_dict)
+result_df.pivot(
+    columns="b", index=["method", "alternative"], values="p-value"
+).reset_index().to_csv("result.csv", index=False)
